9a9d5e7c87e10bac5d265f329aad5006bdf09705
Sihle-eng <smchobokazi@byupathway.edu>
2026-05-22 07:34:18 +0200

    Week 08

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 from app import create_app
 import os
+from werkzeug.middleware.proxy_fix import ProxyFix   # add this import
 
 app = create_app()
+
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
